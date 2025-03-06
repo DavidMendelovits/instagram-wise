24183078e7663ab0e9ea88e4e3ec9b4b78b5b8e8
david Mendelovits <david.mendelovits@gmail.com>
2025-03-06 00:56:20 -0800

    delete replit.nix

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{
-  description = "Node.js environment for Instagram Wise";
-  deps = [
-    pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
-  ];
-} 
